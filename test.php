1ebc5803077fef678a8e8bb5a75259eea9beda73
rickysukma <rickysukma9a@gmail.com>
2020-04-02 10:50:15 +0700

    test heroku server

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+function getData($url){
+    $ch = curl_init(); 
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    // $output contains the output string 
+    $output = curl_exec($ch); 
+
+    // tutup curl 
+    curl_close($ch);      
+
+    // menampilkan hasil curl
+    return $output;
+}
+
+$positif = json_decode(getData('http://covid19.datapedia.id/json/map_prop_positif.php'));
+
+print_r ($positif);
